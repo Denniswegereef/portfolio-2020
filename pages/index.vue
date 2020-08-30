94432e057b908bf2ab6974091c1fb8c7d56e481b
Dennis wegereef <denniswegereef@gmail.com>
2020-08-30 09:56:51 +0200

    trigger again

--- pages/index.vue
+++ pages/index.vue
@@ -17,15 +17,15 @@ export default {
   },
   mounted () {
     const color = '#A7ADA9'
-    const colorRed = '#E10E12'
+    // const colorRed = '#E10E12'
     const tl = this.$data.tl
 
-    gsap.set(this.$refs.heading, { opacity: 0, yPercent: 13 })
-    gsap.set(this.$refs.small, { opacity: 0, y: '-25' })
+    gsap.set(this.$refs.heading, { opacity: 0, yPercent: 5 })
+    gsap.set(this.$refs.small, { opacity: 0, y: '-15' })
 
-    tl.to(this.$refs.intro, { duration: 3, background: color }, 1.1)
-    tl.to(this.$refs.heading, { duration: 2.5, opacity: 1, yPercent: 0, easing: 'Power3.easeInOut' }, 1.2)
-    tl.to(this.$refs.small, { duration: 0.8, opacity: 1, y: 0, easing: 'Power3.easeOut', color: colorRed }, 2.9)
+    tl.to(this.$refs.intro, { duration: 2.5, background: color }, 1.1)
+    tl.to(this.$refs.heading, { duration: 1.5, opacity: 1, yPercent: 0, easing: 'Power3.easeInOut' }, 1.2)
+    tl.to(this.$refs.small, { duration: 1.5, opacity: 1, y: 0, easing: 'Power3.easeOut' }, 1.7)
 
     tl.play()
   },
